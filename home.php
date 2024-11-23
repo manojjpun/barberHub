392909578eae58a60266488eb32ab442a5b85b07
Manoj Pun <manojpun840@gmail.com>
2024-11-23 18:57:17 +0545

    UI for the header completed

---
+++ home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./CSS/home.css">
+</head>
+<body>
+    <div class="home-layout-container">
+        <div class="home-layout">
+            <div class="header-section">
+                <div class="header-left-section">
+                    <span class="logo">barberHub</span>
+                </div>
+                <div class="header-middle-section">
+                    <a class="home-link" href="">Home</a>
+                    <a class="gallery-link" href="">Gallery</a>
+                </div>
+                <div class="header-right-section">
+                    <button class="login-button">Login</button>
+                </div>
+            </div>
+            <div class="middle-section">
+                sdfgdf
+            </div>
+        </div>
+    </div>
+</body>
+</html>
