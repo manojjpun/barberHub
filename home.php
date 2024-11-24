ee9aab2a56cfe83fdd63800e83df79b4cd80bef5
Manoj Pun <manojpun840@gmail.com>
2024-11-24 19:08:04 +0545

    UI for the homepage completed

--- home.php
+++ home.php
@@ -8,22 +8,101 @@
 </head>
 <body>
     <div class="home-layout-container">
-        <div class="home-layout">
-            <div class="header-section">
-                <div class="header-left-section">
-                    <span class="logo">barberHub</span>
-                </div>
-                <div class="header-middle-section">
-                    <a class="home-link" href="">Home</a>
-                    <a class="gallery-link" href="">Gallery</a>
-                </div>
-                <div class="header-right-section">
-                    <button class="login-button">Login</button>
+        <div class="header-section">
+            <div class="header-left-section">
+                <span class="logo">barberHub</span>
+            </div>
+            <div class="header-middle-section">
+                <a class="home-link" href="">Home</a>
+                <a class="gallery-link" href="">Gallery</a>
+            </div>
+            <div class="header-right-section">
+                <button class="login-button" onclick="location.href='login.php'">Login</button>
+            </div>
+        </div>
+        <div class="middle-section">
+            <div class="video-section">
+                <video class="video-showcase" src="./IMAGES/7697129-hd_1920_1080_30fps.mp4" muted loop autoplay></video>
+                <span class="slogan-heading">Snip, Style, Schedule - Your Hair, Your Time.</span>
+                <span class="slogan-description">Say goodbye to waiting! Book your haircut appointments effortlessly with our online barber system.</span>
+                <button class="appointment-button">Make an appointment</button>
+            </div>
+            <div class="our-gallery-section">
+                <span class="our-gallery-heading">Our Gallery</span>
+                <div class="our-gallery">
+                    <div class="gallery-grid">
+                        <img class="gallery-image" src="./IMAGES/_.jpeg" alt="">
+                        <div class="gallery-grid-info">
+                            <div class="gallery-title">
+                                Mullet
+                            </div>
+                            <div class="gallery-description">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa asperiores obcaecati dolores voluptatum, repellendus dolorum. Totam, libero unde molestiae at nesciunt illum id explicabo incidunt numquam similique, eveniet corrupti natus.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gallery-grid">
+                        <img class="gallery-image" src="./IMAGES/_.jpeg" alt="">
+                        <div class="gallery-grid-info">
+                            <div class="gallery-title">
+                                Mullet
+                            </div>
+                            <div class="gallery-description">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa asperiores obcaecati dolores voluptatum, repellendus dolorum. Totam, libero unde molestiae at nesciunt illum id explicabo incidunt numquam similique, eveniet corrupti natus.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gallery-grid">
+                        <img class="gallery-image" src="./IMAGES/_.jpeg" alt="">
+                        <div class="gallery-grid-info">
+                            <div class="gallery-title">
+                                Mullet
+                            </div>
+                            <div class="gallery-description">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa asperiores obcaecati dolores voluptatum, repellendus dolorum. Totam, libero unde molestiae at nesciunt illum id explicabo incidunt numquam similique, eveniet corrupti natus.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="gallery-grid">
+                        <img class="gallery-image" src="./IMAGES/_.jpeg" alt="">
+                        <div class="gallery-grid-info">
+                            <div class="gallery-title">
+                                Mullet
+                            </div>
+                            <div class="gallery-description">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa asperiores obcaecati dolores voluptatum, repellendus dolorum. Totam, libero unde molestiae at nesciunt illum id explicabo incidunt numquam similique, eveniet corrupti natus.
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="middle-section">
-                sdfgdf
+
+            <!-- <div class="our-barbers-section">
+                <span class="our-barbers-heading">Meet your barbers</span>
+                <div class="our-barbers">
+                    <div class="barbers-grid">
+                        <img class="barbers-image" src="./IMAGES/Corte de cabelo masculino, barbear masculino na barbearia _ Foto Premium.jpeg" alt="">
+                        <a class="barbers-link" href="https://www.youtube.com/results?search_query=give+link+on+the+hover+image">
+                            <i class="fa-brands fa-instagram"></i>
+                        </a>
+                    </div>
+                    <div class="barbers-grid">
+                        <img class="barbers-image" src="./IMAGES/Corte de cabelo masculino, barbear masculino na barbearia _ Foto Premium.jpeg" alt="">
+                    </div>
+                    <div class="barbers-grid">
+                        <img class="barbers-image" src="./IMAGES/Corte de cabelo masculino, barbear masculino na barbearia _ Foto Premium.jpeg" alt="">
+                    </div>
+                    <div class="barbers-grid">
+                        <img class="barbers-image" src="./IMAGES/Corte de cabelo masculino, barbear masculino na barbearia _ Foto Premium.jpeg" alt="">
+                    </div>
+                </div>
+            </div> -->
+
+            <div class="footer-section">
+                <span class="footer-logo">barberHub</span>
+                <p class="footer-copyright">© 2024 barberHub. All rights reserved.</p>
             </div>
+
         </div>
     </div>
 </body>
